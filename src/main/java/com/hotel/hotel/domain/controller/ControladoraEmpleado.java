6d94a0e3c06c588e0b43bf3fea8435915de3c6ae
MeJ <don.juncos@gmail.com>
2021-08-24 13:57:56 -0300

    añade visibilidad segun permisos a la barra de navegacion

--- src/main/java/com/hotel/hotel/domain/controller/ControladoraEmpleado.java
+++ src/main/java/com/hotel/hotel/domain/controller/ControladoraEmpleado.java
@@ -4,7 +4,10 @@ import com.hotel.hotel.domain.entity.Empleado;
 import com.hotel.hotel.services.EmpleadoService;
 import java.util.List;
 import javax.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -12,11 +15,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@Slf4j
 public class ControladoraEmpleado {
-	
+
 	@Autowired
 	private EmpleadoService empleadoService;
-	
+
+	@GetMapping("/")
+	public String inicio(Model model,@AuthenticationPrincipal User user) {
+		log.info("USUARIO: "+user.getUsername());
+		return "index";
+	}
+
 	@GetMapping("/listarEmpleados")
 	public String listarEmpleados(Model model) {
 		List<Empleado> empleados = empleadoService.listarEmpleados();
@@ -28,16 +38,18 @@ public class ControladoraEmpleado {
 	public String agregarEmpleado(Empleado empleado) {
 		return "editarEmpleado";
 	}
+
 	@PostMapping("/guardarEmpleado")
 	public String guardarEmpleado(@Valid Empleado empleado, Errors errors) {
-		if(errors.hasErrors()){
+		if (errors.hasErrors()) {
 			return "editarEmpleado";
 		}
 		empleadoService.guardar(empleado);
 		return "redirect:/";
 	}
+
 	@GetMapping("/editarEmpleado/{id}")
-	public String editarEmpleado(Empleado empleado,Model model) {
+	public String editarEmpleado(Empleado empleado, Model model) {
 		empleado = empleadoService.encontrarEmpleado(empleado);
 		model.addAttribute("empleado", empleado);
 		return "editarEmpleado";
