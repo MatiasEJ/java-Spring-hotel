d87b5796c07703ba2b1ab5248afa5847c45bafab
MeJ <don.juncos@gmail.com>
2021-08-24 11:28:12 -0300

    añade manejo de vista de error 403

--- src/main/java/com/hotel/hotel/web/SecurityConfig.java
+++ src/main/java/com/hotel/hotel/web/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.hotel.hotel.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
@@ -20,5 +22,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.password("{noop}123")
 				.roles("USER");
 	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				.antMatchers("/editarEmpleado/**",
+						"/agregarEmpleado/**",
+						"/eliminar")
+					.hasRole("ADMIN")	
+				.antMatchers("/","/listarEmpleados")
+					.hasAnyRole("ADMIN","USER")
+				.and()
+					.formLogin()
+					.loginPage("/login")
+				.and()
+					.exceptionHandling()
+					.accessDeniedPage("/errors/403");
+
+	}
+
+
+
+	
 
 }
